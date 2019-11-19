0481003be8ce3a56a6227c08fd14f09846c7dfb3
Anna Winther <annawinther@annas-mbp.lan>
2019-11-19 13:36:37 +0000

    db added and tables created

---
+++ data/migrations/20191119103654_create-table.js
@@ -0,0 +1,42 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("users", tbl => {
+        tbl.increments()
+        tbl
+            .string("username")
+            .notNullable()
+            .unique();
+        tbl
+            .string("password")
+            .notNullable();
+        tbl
+            .string("email")
+            .notNullable()
+            .unique();
+    })
+    .createTable("jokes", tbl => {
+        tbl.increments()
+        tbl
+            .string("setup")
+            .notNullable();
+        tbl
+            .string("punchline")
+            .notNullable();
+        tbl
+            .string("public")
+            .defaultsTo(true);
+        tbl
+            .integer("user_id")
+            .notNullable()
+            .unsigned()
+            .references("id")
+            .inTable("users");
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists("jokes")
+  .dropTableIfExists("users")
+};
