0d178480c32d3184b44fef3aa32072e7d98adbe1
Anna Winther <annawinther@annas-mbp.lan>
2019-11-21 10:14:27 +0000

    server.spec tests passed

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,7 +3,7 @@ const server = require('./server');
 const request = require('supertest');
 
 describe("GET /", () => {
-    it('has process.env.DB_ENV as "testing', () => {
+    it('has process.env.DB_ENV as "testing"', () => {
         expect(process.env.DB_ENV).toBe("testing")
     })
     it('returns 200 OK', ()=> {
