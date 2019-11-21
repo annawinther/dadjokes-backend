2bf905919b8510e72795ca5c8e507863e7c920c1
Anna Winther <annawinther@annas-mbp.lan>
2019-11-21 14:48:46 +0000

    got jokesRouter tests passing

--- api/routes/jokesRouter.spec.js
+++ api/routes/jokesRouter.spec.js
@@ -10,8 +10,8 @@ beforeEach(async () => {
 
 describe('jokes routes tests', () => {
 
-    describe('GET /', async () => {
-        test('get token back from login', async () => {
+    describe('GET / for all jokes when token is provided', () => {
+        test('get token back from login and can get all jokes', async () => {
             await db('users').insert({
                 username: 'test',
                 password: bcrypt.hashSync('1234', 10),
@@ -20,14 +20,27 @@ describe('jokes routes tests', () => {
             const response =  await request(server)
             .post('/api/auth/login')
             .send({ username: 'test', password: '1234'})
-            .set('Content-Type', 'aaplication/json')
-            // expect(response.body.token).toBeTruthy
 
             const token = response.body.token;
             console.log(token);
-            const response3 = await request(server).get("/api/jokes").set("authorization",token);
-            expect(response3.body.length).toEqual(0);
+            const responseAfterToken = await request(server).get("/api/jokes").set("authorization", token);
+            expect(responseAfterToken.body.length).toEqual(0);
         })
+     })
+
+     describe('tests for 403 FORBIDDEN if no token when hitting jokes endpoints', () => {
+        test("Should return 403 when no token provided", async () => {
+            const responseAllJkes = await request(server).get("/api/jokes");
+            expect(responseAllJkes.status).toBe(403);
+            const responseJokeById = await request(server).get("/api/jokes/:id");
+            expect(responseJokeById.status).toBe(403);
+            const responsePostJoke = await request(server).post("/api/jokes/");
+            expect(responsePostJoke.status).toBe(403);
+            const responseEditJokeById = await request(server).put("/api/jokes/:id");
+            expect(responseEditJokeById.status).toBe(403);
+            const responseDeleteJokeById = await request(server).delete("/api/jokes/:id");
+            expect(responseDeleteJokeById.status).toBe(403);
+        });
      })
        
 })
