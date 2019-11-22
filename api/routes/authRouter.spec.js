4814eba80d3ccb777be31020e3723be2f203bdee
Anna Winther <annawinther@annas-mbp.lan>
2019-11-22 10:16:09 +0000

    some tests changes and api endpoints

--- api/routes/authRouter.spec.js
+++ api/routes/authRouter.spec.js
@@ -14,15 +14,15 @@ describe('auth routes tests', () => {
     describe('POST /register',  () => {
         it('should regster a new user', async () => {
             const newUser = await request(server)
-            .post('/api/auth/register')
-            .send({ username: 'zara', password: '1234', email: 'zara@test.com'});
-            expect(newUser.body.username).toMatch(/zara/)
+            .post('/auth/register')
+            .send({ username: 'hanna', password: '1234', email: 'hanna@test.com'});
+            expect(newUser.body.username).toMatch(/hanna/)
         })
 
         it('should return a status of 201', async () => {
             const response = await request(server)
-            .post('/api/auth/register')
-            .send({ username: 'zara', password: '1234', email: 'zara@test.com' })
+            .post('/auth/register')
+            .send({ username: 'hanna', password: '1234', email: 'hanna@test.com' })
             expect(response.status).toBe(201)
         })
     })
@@ -30,24 +30,24 @@ describe('auth routes tests', () => {
     describe('POST /login', () => {
         it('should return 200 OK', async () => {
             await db('users').insert({
-                username: 'zara',
+                username: 'hanna',
                 password: bcrypt.hashSync('1234', 10),
-                email: 'zara@test.com',
+                email: 'hanna@test.com',
             })
             const response =  await request(server)
-            .post('/api/auth/login')
-            .send({ username: 'zara', password: '1234'})
+            .post('/auth/login')
+            .send({ username: 'hanna', password: '1234'})
             expect(response.status).toBe(200)
         })
         it('should get token back', async () => {
             await db('users').insert({
-                username: 'zara',
+                username: 'hanna',
                 password: bcrypt.hashSync('1234', 10),
-                email: 'zara@test.com'
+                email: 'hanna@test.com'
             })
             const response =  await request(server)
-            .post('/api/auth/login')
-            .send({ username: 'zara', password: '1234'})
+            .post('/auth/login')
+            .send({ username: 'hanna', password: '1234'})
             .set('Content-Type', 'aaplication/json')
             expect(response.body.token).toBeTruthy
         })
